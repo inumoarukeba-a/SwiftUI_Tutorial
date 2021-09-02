f0ff72613fa2bfc8ef74f93caf70edbe65d07839
Ataru Nakano <atarunakano@gmail.com>
2021-09-03 08:11:51 +0900

    https://developer.apple.com/tutorials/swiftui/creating-and-combining-views

---
+++ Landmark/Landmark/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  CircleImage
+//
+//  Created by Ataru Nakano on 2021/09/02.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle()
+                        .stroke(Color.gray, lineWidth: 4))
+            .shadow(radius: /*@START_MENU_TOKEN@*/7/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ Landmark/Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Ataru Nakano on 2021/09/02.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
